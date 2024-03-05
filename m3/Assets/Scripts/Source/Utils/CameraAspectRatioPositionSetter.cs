97b60be3adc573c0817d7c6c5d1fbcd4c9fdef94
oldbard <oldbard@gmail.com>
2024-03-05 10:55:39 +0100

    Adds an utils assembly and some classes to help update anchoring when changing orientation

---
+++ m3/Assets/Scripts/Source/Utils/CameraAspectRatioPositionSetter.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace OldBard.Match3.Gameplay.Views
+{
+	[RequireComponent(typeof(Camera))]
+	public class CameraAspectRatioPositionSetter : MonoBehaviour
+	{
+		[SerializeField] Camera _camera;
+		[SerializeField] Vector2 _referenceResolution;
+		[SerializeField] float _referenceXPosition;
+
+		float _referenceRatio;
+
+		void Awake()
+		{
+			_referenceRatio = GetReferenceRation();
+
+			UpdatePosition();
+		}
+
+		void UpdatePosition()
+		{
+			Transform trans = transform;
+
+			Vector3 currentPosition = trans.localPosition;
+
+			trans.localPosition = new Vector3(GetXPosition(), currentPosition.y, currentPosition.z);
+		}
+
+		float GetXPosition()
+		{
+			var ratio = _camera.aspect / _referenceRatio;
+
+			return _referenceXPosition * ratio;
+		}
+
+		float GetReferenceRation()
+		{
+			return _referenceResolution.x / _referenceResolution.y;
+		}
+
+#if UNITY_EDITOR
+		public void UpdateEditorPosition()
+		{
+			_referenceRatio = GetReferenceRation();
+
+			UpdatePosition();
+		}
+
+		public float GetAspectRatio()
+		{
+			return _camera.aspect;
+		}
+
+		public float GetCalculatedX()
+		{
+			return GetXPosition();
+		}
+#endif
+	}
+}
---
+++ m3/Assets/Scripts/Source/Utils/OrientationAnchorUpdater.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+namespace OldBard.Match3.Gameplay.Views
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class OrientationAnchorUpdater : MonoBehaviour
+    {
+        [SerializeField] Vector2 _landscapePivot = new(0f, 0.5f);
+        [SerializeField] Vector2 _landscapeMinMax = new(0f, 0.5f);
+        [SerializeField] Vector2 _landscapeOffSet = Vector2.zero;
+
+        [SerializeField] Vector2 _portraitPivot = new(0.5f, 1f);
+        [SerializeField] Vector2 _portraitMinMax = new(0.5f, 1f);
+        [SerializeField] Vector2 _portraitOffSet = Vector2.zero;
+
+        [SerializeField] RectTransform _rectTransform;
+
+        ScreenOrientation _currentOrientation = 0;
+
+        void Awake()
+        {
+            UpdateOrientation();
+        }
+
+        void OnEnable()
+        {
+            UpdateOrientation();
+        }
+        
+        void OnRectTransformDimensionsChange()
+        {
+            UpdateOrientation();
+        }
+
+        void UpdateOrientation()
+        {
+            ScreenOrientation orientation = Screen.orientation;
+
+#if UNITY_EDITOR
+            orientation = GetEditorTimeOrientation();
+#endif
+            if (orientation == _currentOrientation)
+            {
+                return;
+            }
+
+            _currentOrientation = orientation;
+
+            RefreshOrientation(orientation);
+        }
+
+        void RefreshOrientation(ScreenOrientation orientation)
+        {
+            switch(orientation)
+            {
+                case ScreenOrientation.Portrait or ScreenOrientation.PortraitUpsideDown:
+                    _rectTransform.anchorMin = _portraitMinMax;
+                    _rectTransform.anchorMax = _portraitMinMax;
+                    _rectTransform.pivot = _portraitPivot;
+                    _rectTransform.ForceUpdateRectTransforms();
+
+                    _rectTransform.anchoredPosition = _portraitOffSet;
+                    break;
+                case ScreenOrientation.LandscapeLeft or ScreenOrientation.LandscapeRight:
+                    _rectTransform.anchorMin = _landscapeMinMax;
+                    _rectTransform.anchorMax = _landscapeMinMax;
+                    _rectTransform.pivot = _landscapePivot;
+                    _rectTransform.ForceUpdateRectTransforms();
+
+                    _rectTransform.anchoredPosition = _landscapeOffSet;
+                    break;
+            }
+        }
+
+#if UNITY_EDITOR
+        public void ForceRefreshOrientation(ScreenOrientation orientation)
+        {
+            RefreshOrientation(orientation);
+        }
+
+        ScreenOrientation GetEditorTimeOrientation()
+        {
+            return Screen.width > Screen.height ? ScreenOrientation.LandscapeLeft : ScreenOrientation.Portrait;
+        }
+#endif
+    }
+}
